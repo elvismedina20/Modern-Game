e2c4294dd7c5c8ff49fb97c75e1d018eb91f230c
elvismedina20 <elvismedina1011@gmail.com>
2025-04-15 19:42:21 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,36 +1,41 @@
-class Player {
-  constructor(id, side) {
-    this.el = document.getElementById(id);
-    this.side = side;
-    this.aim = 0;
-    this.speed = 0;
-    this.position = [this.el.offsetLeft, this.el.offsetTop];
-  }
-
-  move(dy) {
-    const newY = this.el.offsetTop + dy;
-    const maxY = document.getElementById("game").offsetHeight - this.el.offsetHeight;
-    const clampedY = Math.max(0, Math.min(newY, maxY));
-    this.el.style.top = clampedY + "px";
-    this.position[1] = clampedY;
-  }
-
-  setAim(direction) {
-    this.aim = direction;
-  }
-
-  fire() {
-    if (ball.owner === this) {
-      ball.velocity = [this.side === "left" ? 0.5 : -0.5, this.aim * 0.5];
-      ball.owner = null;
+export class Player {
+    constructor(id, side, game, ball) {
+        this.el = document.getElementById(id);
+        this.side = side;
+        this.height = 100;
+        this.width = 15;
+        this.speed = 6;
+        this.y = (game.clientHeight - this.height) / 2;
+        this.el.style.top = `${this.y}px`;
+        this.movingUp = false;
+        this.movingDown = false;
+        this.game = game;
+        this.ball = ball;
     }
-  }
 
-  getPosition() {
-    return [this.el.offsetLeft, this.el.offsetTop];
-  }
+    getCenterY() {
+        return this.y + this.height / 2;
+    }
 
-  getSide() {
-    return this.side;
-  }
+    move(dy) {
+        this.y = Math.max(0, Math.min(this.y + dy, this.game.clientHeight - this.height));
+        this.el.style.top = `${this.y}px`;
+    }
+
+    update(deltaTime, keysPressed) {
+        let dy = 0;
+        if (this.side === "left") {
+            if (keysPressed['w']) dy = -this.speed;
+            if (keysPressed['s']) dy = this.speed;
+        }
+        this.move(dy);
+    }
+
+    fire() {
+        if (this.ball.owner === this) {
+            const direction = this.side === "left" ? 1 : -1;
+            this.ball.velocity = [direction * this.ball.baseSpeed, (Math.random() - 0.5) * 5];
+            this.ball.owner = null;
+        }
+    }
 }
